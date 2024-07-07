b5f56ce51cc57fd733acce1ad41b1091945aa5a2
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-07-07 15:41:17 +1200

    herobox-withoutbutton

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,18 +3,24 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        cantora: ['Cantora One', 'sans-serif'],
+        lilita: ['Lilita One', 'sans-serif'],
+      },
       colors: {
         darkPink: '#653C4B',
         lightGrey: '#EEEEEE',
         brightPink: '#FF71B5',
         dirtyPink: '#c27fbe',
+        darkGrey: '#2c2c2c',
       },
       backgroundImage: {
-        gradientPink: 'linear-gradient(to right, #C4A4A5, #FFEAE5)',
+        gradientPink: 'linear-gradient(to right, #9B797A, #FFEAE5)',
       },
       spacing: {
         55: '55%',
         35: '135px',
+        te: '850px',
       },
     },
   },
